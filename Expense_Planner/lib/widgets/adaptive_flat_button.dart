5e889d6991f9a6b31513057b21fece200f077152
chathu1997 <43296991+chathu1997@users.noreply.github.com>
2020-09-07 11:24:31 +0530

    initial commit

---
+++ Expense_Planner/lib/widgets/adaptive_flat_button.dart
@@ -0,0 +1,33 @@
+import 'dart:io'; //to import platform class
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class AdaptiveFlatButton extends StatelessWidget {
+  final String text;
+  final Function handler;
+
+  AdaptiveFlatButton(this.text, this.handler);
+
+  @override
+  Widget build(BuildContext context) {
+    //print('build() AdaptiveFlatButton');
+    return Platform.isIOS
+        ? CupertinoButton(
+            //color: Colors.blue, //choose date button becomes blue color one
+            child: Text(
+              text,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            onPressed: handler,
+          )
+        : FlatButton(
+            textColor: Theme.of(context).primaryColor,
+            child: Text(
+              text,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            onPressed: handler,
+          );
+  }
+}
